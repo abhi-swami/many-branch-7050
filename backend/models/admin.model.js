3f767347bbf3c00f0e286be863765b00793efa69
shivammchaudhary1 <shivamchaudhary75@gmail.com>
2023-05-04 22:30:20 +0530

    Admin Added

---
+++ backend/models/admin.model.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const adminSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    mobile: { type: Number, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+  },
+  { versionKey: false }
+);
+
+const AdminModel = model("admin", adminSchema);
+
+module.exports = { AdminModel };
