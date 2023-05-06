111694e1017638aa54e4f14c61e0118ec3d91c3b
abhi-swami <manjuswami338@gmail.com>
2023-05-07 02:25:12 +0530

    cart routes

---
+++ backend/controller/cart.controller.js
@@ -0,0 +1,60 @@
+const { CartModel } = require("../models/cart.model");
+
+const postCartProduct = async (req, res) => {
+  let obj = req.body;
+  try {
+    const isProductExist= await CartModel.find(obj)
+    if(isProductExist.length>0){
+      newData=isProductExist[0];
+      const arr=isProductExist.map((el)=>el.quantity?el.quantity+1:el)
+      newData.quantity=arr[0]
+      const data = await CartModel.findByIdAndUpdate(newData._id, newData, {
+        returnDocument: "after",
+      });
+      res.status(200).send({ mesg: `Product Added To cart`,data});
+    }else{
+      const userPost = new CartModel(obj);
+      await userPost.save();
+      res.status(200).send({ mesg: `Product Added To cart`, data: userPost });
+    }
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+};
+const getCartProduct = async (req, res) => {
+  const filter = {};
+  for (let i in req.body) {
+    if (i === "userID") {
+      filter.userID = req.body[i];
+    }
+  }
+  try {
+    const data = await CartModel.find(filter);
+    res.status(200).send({ data: data });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+};
+const patchCartProduct = async (req, res) => {
+    const { id } = req.params;
+    let obj = req.body;
+    try {
+        const data = await CartModel.findByIdAndUpdate(id, obj, {
+            returnDocument: "after",
+        });
+        console.log(data);
+        res.status(200).send({ mesg: `Updated the post successfully`, data: data });
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+};
+const deleteCartProduct = async (req, res) => {
+    const { id } = req.params;
+    try {
+      const data = await CartModel.findByIdAndDelete(id);
+      res.status(200).send({ mesg: `Deleted the post successfully`,data:data});
+    } catch (error) {
+      res.status(400).send({ error: error.message});
+    }
+  }
+module.exports = { postCartProduct, getCartProduct, patchCartProduct,deleteCartProduct };
--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -20,25 +20,26 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-  const token = req.headers.authorization;
-
-  if (!token) {
-    return res.status(401).send({ error: "Unauthorized" });
-  }
-
-  try {
-    const decoded = jwt.verify(token.split(" ")[1], "amaze");
-    console.log(decoded);
-
-    if (!decoded) {
-      return res.status(401).send({ error: "Unauthorized" });
+  const url = req.url;
+  const {token} = req.headers;
+  if (url.includes("cart")) {
+    if (!token) {
+      return res.status(401).send({ error: "token is missing" });
     }
+    try {
+      const decoded = jwt.verify(token, "amaze");
+      if (!decoded) {
+        return res.status(401).send({ error: "Unauthorized" });
+      }
+      if(decoded){
+        req.body.userID = decoded.userID;
+      }
+      //relationship form here
 
-    //relationship form here
-
-    next();
-  } catch (error) {
-    res.status(401).send({ error: "Unauthorized" });
+      next();
+    } catch (error) {
+      res.status(401).send({ error: "Unauthorized" });
+    }
   }
 };
 
